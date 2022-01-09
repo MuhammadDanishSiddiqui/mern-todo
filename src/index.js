d476c8fbb4689c01d648a3d4cc7e7dbfe28972e8
danish siddiqui <danishsiddiqui6789@gmail.com>
2022-01-10 02:14:39 +0500

    bugs fixing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 
 var cors = require('cors')
-const express=require("express")
+const express = require("express")
 const app = express()
+const path = require("path")
 const userRouter = require("./routes/user")
 const taskRouter = require("./routes/tasks")
-const dotenv = require ("dotenv")
+const dotenv = require("dotenv")
 dotenv.config()
 require("./db/conn")
 
@@ -16,16 +17,16 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+const staticPath = path.join(__dirname, "../client/build")
 
-if(process.env.NODE_ENV == "production")
-{
-    app.use(express.static("client/build"));
-    const path = require("path")
-    app.get("*",(req,res)=>{
-        res.sendfile(path.resolve(__dirname,"client","build","index.html"))
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(staticPath));
+    app.get("*", (req, res) => {
+        res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"))
     })
 }
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("listening to port no " + port)
 })
